4cae1950c935f712a0fca46bdbf608e479dd13eb
Reiland, Cohen <cohen200521@gmail.com>
2026-04-22 10:17:18 -0500

    Added Firebase Connection

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth/web-extension';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyASjVVhHYcJhDiMo30woJqyD34UKewvppU',
+  authDomain: 'frameworks-final-project.firebaseapp.com',
+  projectId: 'frameworks-final-project',
+  storageBucket: 'frameworks-final-project.firebasestorage.app',
+  messagingSenderId: '198806295938',
+  appId: '1:198806295938:web:e8348cd6f9745596a92b9b',
+  measurementId: 'G-DHGSTJLMNY',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+const analytics = getAnalytics(app);
