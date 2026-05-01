30f73951959178581f69177c56bd7e8fe9372aa0
Reiland, Cohen <cohen200521@gmail.com>
2026-04-30 21:10:27 -0500

    Added Basic html Structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { SignUpComponent } from './sign-up-component/sign-up-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'signup',
+    component: SignUpComponent,
+    title: 'Sign Up',
+  },
+];
