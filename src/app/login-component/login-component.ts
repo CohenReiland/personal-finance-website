86be32faa33bce7c84e668cfe003711008025c2e
Reiland, Cohen <cohen200521@gmail.com>
2026-05-03 16:57:28 -0500

    Added RouterLinks

--- src/app/login-component/login-component.ts
+++ src/app/login-component/login-component.ts
@@ -1,11 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../services/auth-service';
 
 @Component({
   selector: 'app-login-component',
-  imports: [ReactiveFormsModule],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login-component.html',
   styleUrl: './login-component.css',
 })
