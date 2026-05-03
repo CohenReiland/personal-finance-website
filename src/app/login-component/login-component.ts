a4744e24859fab9a453af6ae62f5e5371a1ed0a2
Reiland, Cohen <cohen200521@gmail.com>
2026-05-03 16:40:54 -0500

    Added Form and Variables

--- src/app/login-component/login-component.ts
+++ src/app/login-component/login-component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth-service';
 
 @Component({
   selector: 'app-login-component',
@@ -7,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrl: './login-component.css',
 })
 export class LoginComponent {
+  private readonly formBuilder = inject(FormBuilder);
+  private readonly router = inject(Router);
+  private readonly authService = inject(AuthService);
+
+  // Variables to hold states 
+  protected showPassword = false;
+  protected submitted = false;
+  protected loginError = '';
+  protected readonly isLoading = this.authService.isLoading;
+
+  // Form that holds user login input
+  protected readonly loginForm = this.formBuilder.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
 
 }
