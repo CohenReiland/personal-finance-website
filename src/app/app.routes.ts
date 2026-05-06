575a71359bff0b471d72fa60258b12f8d8cd0112
Reiland, Cohen <cohen200521@gmail.com>
2026-05-06 18:16:29 -0500

    Added resolveAuthUser Function

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,17 @@ import { Routes } from '@angular/router';
 import { SignUpComponent } from './sign-up-component/sign-up-component';
 import { LoginComponent } from './login-component/login-component';
 import { Dashboard } from './pages/dashboard/dashboard';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase.config';
+
+// Function that checks to see if a user is currently logged in one time
+const resolveAuthUser = async (): Promise<boolean> =>
+  new Promise((resolve) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      unsubscribe();
+      resolve(Boolean(user));
+    });
+  });
 
 export const routes: Routes = [
   {
@@ -18,5 +29,5 @@ export const routes: Routes = [
     path: 'dashboard',
     component: Dashboard,
     title: 'Dashboard',
-  }
+  },
 ];
