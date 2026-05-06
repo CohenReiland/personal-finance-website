300c3eeeaeedbc3b02d11d7f1e9ab9d6059a55dd
Reiland, Cohen <cohen200521@gmail.com>
2026-05-06 18:18:36 -0500

    Added authGuard and guestGuard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
-import { Routes } from '@angular/router';
+import { CanActivateFn, Router, Routes } from '@angular/router';
 import { SignUpComponent } from './sign-up-component/sign-up-component';
 import { LoginComponent } from './login-component/login-component';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from './firebase.config';
+import { inject } from '@angular/core';
 
 // Function that checks to see if a user is currently logged in one time
 const resolveAuthUser = async (): Promise<boolean> =>
@@ -13,6 +14,22 @@ const resolveAuthUser = async (): Promise<boolean> =>
       resolve(Boolean(user));
     });
   });
+
+// Guard that only allows authenticated users to access routes if not redirects to login
+const authGuard: CanActivateFn = async () => {
+  const router = inject(Router);
+  const isAuthenticated = await resolveAuthUser();
+
+  return isAuthenticated ? true : router.createUrlTree(['/login']);
+};
+
+// Guard that only allows guests to access routes if not authenticated redirects to dashboard
+const guestGuard: CanActivateFn = async () => {
+  const router = inject(Router);
+  const isAuthenticated = await resolveAuthUser();
+
+  return isAuthenticated ? router.createUrlTree(['/dashboard']) : true;
+};
 
 export const routes: Routes = [
   {
