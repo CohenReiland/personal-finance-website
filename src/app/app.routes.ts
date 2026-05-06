13a7da283b2abfa25c65e8d86e39b65309e912d7
aiden123 <aidensagaser@gmail.com>
2026-05-05 23:50:33 -0500

    working app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,6 @@ import { TransactionList } from './transaction-list/transaction-list';
 
 export const routes: Routes = [
   {
-    path: '', //this rediects the default path to login
-    redirectTo: 'login',
-    pathMatch: 'full',
-  },
-  {
     path: 'signup',
     component: SignUpComponent,
     title: 'Sign Up',
