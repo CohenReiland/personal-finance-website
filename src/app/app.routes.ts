42968a3332cd8a52dd8da21b1e6ae08af140568e
Daemoj <ethan@cuyunaverse.com>
2026-05-06 14:31:06 -0500

    added dashboard html (to change), adjusted routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { SignUpComponent } from './sign-up-component/sign-up-component';
 import { LoginComponent } from './login-component/login-component';
+import { Dashboard } from './pages/dashboard/dashboard';
 
 export const routes: Routes = [
   {
@@ -9,8 +10,13 @@ export const routes: Routes = [
     title: 'Sign Up',
   },
   {
-    path: 'login',
+    path: '',
     component: LoginComponent,
     title: 'Login',
   },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+    title: 'Dashboard',
+  }
 ];
